9a44a755dd4fbc6917d333181c3c0d407f212371
Naufal <mnaufalawalludin10@gmail.com>
2026-05-20 12:29:24 +0700

    fix: webpack externals for prisma

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactCompiler: true,
   serverExternalPackages: ['@prisma/client', 'prisma'],
   images: {
     domains: ["lh3.googleusercontent.com"]
+  },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.externals = [
+        ...(config.externals || []),
+        '@prisma/client',
+        'prisma'
+      ]
+    }
+    return config
   }
 };
 
