f34ec2e4e6990bc2f5d0671684efda4dd80b2a19
Naufal <mnaufalawalludin10@gmail.com>
2026-05-21 16:54:06 +0700

    fix: move styles to globals.css

--- app/layout.js
+++ app/layout.js
@@ -7,6 +7,7 @@ import Navbar from "../components/Navbar";
 import { SessionProvider } from "next-auth/react";
 import { CartProvider } from "../context/CartContext";
 import { AuthProvider } from "../context/AuthContext";
+import NavbarWrapper from "../components/NavbarWrapper";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -32,7 +33,7 @@ export default function RootLayout({ children }) {
         <Providers>
           <AuthProvider>
             <CartProvider>
-              <Navbar/>
+              <NavbarWrapper/>
               {children}
             </CartProvider>
           </AuthProvider>
