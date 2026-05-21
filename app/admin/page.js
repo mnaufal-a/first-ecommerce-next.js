58e73f83be6512a4e7c2d9374fd56f78025efd70
Naufal <mnaufalawalludin10@gmail.com>
2026-05-21 10:53:12 +0700

    fix: separate authOptions to lib/auth.js

--- app/admin/page.js
+++ app/admin/page.js
@@ -1,6 +1,6 @@
 
 import { getServerSession } from "next-auth";
-import { authOptions } from "../api/auth/[...nextauth]/route";
+import { authOptions } from "@/lib/auth"
 import { redirect } from "next/navigation";
 import AdminForm from "./AdminForm";
 import { prisma } from "../../lib/prisma";
--- app/api/products/route.js
+++ app/api/products/route.js
@@ -1,6 +1,6 @@
 import { prisma } from "../../../lib/prisma";
 import { getServerSession } from "next-auth";
-import { authOptions } from "../auth/[...nextauth]/route";
+import { authOptions } from "@/lib/auth"
 
 export async function GET(req) {
     try {
