f4675166b035f26d72bea5d412fc5fa169916954
Naufal <mnaufalawalludin10@gmail.com>
2026-05-21 10:45:54 +0700

    fix: separate authOptions to lib/auth.js

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,72 +1,73 @@
 import NextAuth from "next-auth";
-import GoogleProvider from "next-auth/providers/google";
-import { PrismaAdapter } from "@next-auth/prisma-adapter"
+import { authOptions } from "../../../../lib/auth";
+// import GoogleProvider from "next-auth/providers/google";
+// import { PrismaAdapter } from "@next-auth/prisma-adapter"
 // import { PrismaClient } from "@prisma/client"
 
 // const prisma = new PrismaClient()
 console.log("NEXTAUTH_URL:", process.env.NEXTAUTH_URL)
 
-export const authOptions = {
-  adapter: PrismaAdapter(prisma),
+// export const authOptions = {
+//   adapter: PrismaAdapter(prisma),
 
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+//   providers: [
+//     GoogleProvider({
+//       clientId: process.env.GOOGLE_CLIENT_ID,
+//       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
 
-      async profile(profile) {
-        return {
-          id: profile.sub,
-                name: profile.name,
-                email: profile.email,
-                image: profile.picture,
+//       async profile(profile) {
+//         return {
+//           id: profile.sub,
+//                 name: profile.name,
+//                 email: profile.email,
+//                 image: profile.picture,
 
-                role: "ADMIN"
-        }
-      }
-    }),
-  ],
+//                 role: "ADMIN"
+//         }
+//       }
+//     }),
+//   ],
 
-  session: {
-    strategy: "jwt",
-  },
+//   session: {
+//     strategy: "jwt",
+//   },
 
-  callbacks: {
-  async jwt({ token, user }) {
-  // ⬅️ saat login pertama
-    if (user) {
-      token.role = user.role || "USER"
-    }
+//   callbacks: {
+//   async jwt({ token, user }) {
+//   // ⬅️ saat login pertama
+//     if (user) {
+//       token.role = user.role || "USER"
+//     }
 
-  // ⬅️ ambil dari DB setiap request
-    if (token.email) {
-      const dbUser = await prisma.user.findUnique({
-        where: { email: token.email }
-      })
+//   // ⬅️ ambil dari DB setiap request
+//     if (token.email) {
+//       const dbUser = await prisma.user.findUnique({
+//         where: { email: token.email }
+//       })
 
-      token.role = dbUser?.role ?? "USER"
-    }
+//       token.role = dbUser?.role ?? "USER"
+//     }
 
-    console.log("ROLE FINAL:", token.role)
+//     console.log("ROLE FINAL:", token.role)
 
-    return token
-  },
+//     return token
+//   },
 
-  async session({ session, token }) {
+//   async session({ session, token }) {
   
-    if (!session.user) session.user = {}
+//     if (!session.user) session.user = {}
 
-    session.user.role = token.role ?? "USER"
+//     session.user.role = token.role ?? "USER"
 
-    return session
-  }
-},
-}
+//     return session
+//   }
+// },
+// }
 
-console.log(
-  "GOOGLE CALLBACK:",
-  `${process.env.NEXTAUTH_URL}/api/auth/callback/google`
-)
+// console.log(
+//   "GOOGLE CALLBACK:",
+//   `${process.env.NEXTAUTH_URL}/api/auth/callback/google`
+// )
 
 const handler = NextAuth(authOptions)
 
--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -1,6 +1,6 @@
 import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
-import { authOptions } from "../api/auth/[...nextauth]/route";
+import { authOptions } from "../../lib/auth";
 import Link from "next/link";
 import LogoutButton from "./LogoutButton";
 import { 
